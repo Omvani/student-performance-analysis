30f4b0d8a29ec365c20b51dda7a73e2ec864b204
Om Vani <omvani776@gmail.com>
2025-12-29 11:16:34 +0530

    Done

--- src/model.py
+++ src/model.py
@@ -1,13 +1,32 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error,r2_score
 from preprocessing import preprocess_data
+import matplotlib.pyplot as plt
 def train_test_evaluate():
     X_train,X_test,y_train,y_test=preprocess_data("D:/project-1/student-performance-analysis/DATA/STUDENT_DATASET.csv")
-    model=LinearRegression()
+    model=Pipeline([
+        ('poly',PolynomialFeatures(degree=2)),
+        ('lr',LinearRegression())
+    ])
+    # model=RandomForestRegressor(
+    #     n_estimators=200,
+    #     random_state=42
+    # )
     model.fit(X_train,y_train)
     y_pred=model.predict(X_test)
     mse=mean_squared_error(y_test,y_pred)
     r2=r2_score(y_test,y_pred)
     print("Mean Squared Error=",mse)
     print("R2 Score=",r2)
+    plt.figure(figsize=(7,5))
+    plt.scatter(y_test,y_pred)
+    plt.xlabel("Actual score")
+    plt.ylabel("Predicted")
+    plt.title("Actual vs Predicted")
+    plt.plot([y_test.min(),y_test.max()],
+             [y_test.min(),y_test.max()])
+    plt.show()
 train_test_evaluate()
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,6 +3,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 def load_data(path):
     return pd.read_csv(path)
+def add_column(data):
+    data['effort_score']=(data['study_hours']*0.4+data['assignment']*0.3+data['internal_marks']*0.3)
 def split_target_features(data):
     X=data.drop("final_score",axis=1)
     y=data["final_score"]
@@ -16,6 +18,7 @@ def scale_features(X_train,X_test):
     return X_train_scaled,X_test_scaled
 def preprocess_data(path):
     data=load_data(path)
+    add_column(data)
     X,y=split_target_features(data)
     X_train,X_test,y_train,y_test=split_test_train(X,y)
     X_train_scaled,X_test_scaled=scale_features(X_train,X_test)
